fa6ce8e58fa4e9f955918f4e3a15886119800bab
art0s-dev <art0s@mailbox.org>
2022-06-26 15:36:25 +0200

    TODO: Added Some Comments to the Stack

--- src/public/main.php
+++ src/public/main.php
@@ -7,7 +7,9 @@ error_reporting(E_ERROR | E_WARNING | E_ALL);
 #Get Autoloader
 require dirname(dirname(dirname(__FILE__))) . './vendor/autoload.php';
 
+#Import Statements
 use Wepkid\core\test;
+
 $test = new test();
 
 // echo '<pre>';
